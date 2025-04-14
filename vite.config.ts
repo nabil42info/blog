2510c42d10c3edb5990a5dd87fbc00db50042a9d
nabil42info <nabil42.informatique@gmail.com>
2025-04-14 21:19:00 +0200

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+ server: {
+    port: process.env.PORT || 5173,
+    host: true // important pour éviter le "use --host to expose"
+  }
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
